bfc9534a9a398daea283472ae156717d5154df8a
Miguellamacarrasco <63501373+Miguellamacarrasco@users.noreply.github.com>
2020-11-11 21:47:59 -0500

    some

--- head.h
+++ head.h
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
+#include <queue>
+#include <set>
 #ifndef head
 #define head
 
@@ -17,11 +20,21 @@ struct node
 };
 
 template<typename vocabulary>
+struct meta_node
+{
+    int key;
+    std::set<node<vocabulary>*> identity;
+    std::unordered_map<vocabulary,std::set<node<vocabulary>*>> adjacent;
+    std::unordered_map<vocabulary,int> adjacent_key;
+};
+
+template<typename vocabulary>
 struct automata
 {
     node<vocabulary>* initial_state;
     std::unordered_map<int, node<vocabulary>*> final_states;
     std::unordered_map<int, node<vocabulary>*> states;
+    std::vector<vocabulary> possible_inputs = {'0','1','e'};
 
     void add_state(int key)
     {
@@ -34,67 +47,215 @@ struct automata
         states[key1]->adjacent.push_back(std::pair<vocabulary,node<vocabulary>*>(t_input,states[key2]));
     }
     
-    void remove_state(int key) //O(4n)
+    void remove_state(int key) 
     {
-        for (auto & state : states) //O(n)
+        for (auto & state : states)
         {
             int i = 0;
-            for (auto & things : state.second->adjacent) //O(2)
+            for (auto & things : state.second->adjacent) 
             {
-                if (things.second == states[key]) //O(1)
+                if (things.second == states[key])
                 {
                     state.second->adjacent.erase(state.second->adjacent.begin() + i);
                 }
+                else
+                {
+                    i++;
+                }
+                
             }
         }
-        if (initial_state == states[key]) //O(1)
+        if (initial_state == states[key]) 
         {
             initial_state = nullptr;
         }
-        states.erase(key); //O(n)
-        final_states.erase(key); //O(n)
+        states.erase(key); 
+        final_states.erase(key);
     }
 
-    node<vocabulary>* operator[](int key) //O(1)
+    node<vocabulary>* operator[](int key) 
     {
         return states[key];
     }
 
-    void Brzozowski()
+    void nfa_to_dfa(std::vector<vocabulary> possible)
     {
-        //Identify nodes that are unreachable from the intial state and delete them 
-        std::vector<node<vocabulary>*> to_visit;
-        std::unordered_map<int,bool> found;
-        for (auto state : states) //O(n)
+        std::vector<meta_node<vocabulary>*> tabla;
+        std::vector<meta_node<vocabulary>*> needs_to_be_declared;
+        meta_node<vocabulary>* zero_meta = new meta_node<vocabulary>;
+        zero_meta->identity.insert(initial_state);
+        zero_meta->key = 0;
+        for (auto change : possible)
         {
-            found.insert({state.second->key,false});
-        }
-        to_visit.push_back(initial_state);
-        while (!to_visit.empty()) //O(n)
-        {
-            for (auto adjacent : to_visit[0]->adjacent) //O(2) 
+            std::set<node<vocabulary>*> vector_of_change;
+            for (auto node : states[-1]->adjacent)
             {
-                if (!found[adjacent.second->key]) //O(1)
+                if (node.first == change) //This is bad code
                 {
-                    to_visit.push_back(adjacent.second);
-                    found[adjacent.second->key] = true;
+                    vector_of_change.insert(node.second);
                 }
             }
-            to_visit.erase(to_visit.begin());
+            zero_meta->adjacent.insert({change,vector_of_change});
         }
-        for (auto estado : found)  //O(n) 
+        needs_to_be_declared.push_back(zero_meta);
+        int new_key = 1;
+        while (!needs_to_be_declared.empty())
         {
-            if (!estado.second)
+            meta_node<vocabulary>* meta = needs_to_be_declared.front();
+            //Search Adjacent
+            for (auto possible_change : possible)
             {
-                //This state is unacessible, therefore it should be deleted 
-                remove_state(estado.first); //O(4n)
+                bool needs_to_be_declared_bool = true;
+                for (auto identity_elements : meta->identity)
+                {
+                    
+                    for (auto elments_adjacent : identity_elements->adjacent) //Search possible coincidences
+                    {
+                        if (elments_adjacent.first == possible_change)
+                        {
+                            meta->adjacent[possible_change].insert(elments_adjacent.second);
+                        }
+                    }
+                }
+
+
+
+
+                //Look of its adjacent needs to be declared
+                    for (auto meta_in_table : tabla)
+                    {
+                        if (meta_in_table->identity == meta->adjacent[possible_change])
+                        {
+                            needs_to_be_declared_bool = false;
+                        }
+                    }
+                    //Look if its already been added to the Needs_to_be_declared
+                    for (auto meta_in_needs_to_be_declared : needs_to_be_declared)
+                    {
+                        if (meta_in_needs_to_be_declared->identity == meta->adjacent[possible_change])
+                        {
+                            needs_to_be_declared_bool = false;
+                        }
+                    }
+
+                    //Adds it to the list of things that need declaration
+                    if (needs_to_be_declared_bool)
+                    {
+                        meta_node<vocabulary>* new_declaration = new meta_node<vocabulary>;
+                        new_declaration->identity =  meta->adjacent[possible_change];
+                        new_declaration->key = new_key;
+                        new_key++;
+                        needs_to_be_declared.push_back(new_declaration);
+                    }
+
+            }
+            
+            tabla.push_back(meta);
+            needs_to_be_declared.erase(needs_to_be_declared.begin());
+        }
+        //So now I need to convert the meta nodes to normal nodes
+        std::unordered_map<int,node<vocabulary>*> new_nodos;
+        std::unordered_map<int,node<vocabulary>*> new_finales;
+        node<vocabulary>* new_inicial;
+        for (auto meta_nodo : tabla)
+        {
+            node<vocabulary>* nuevo_nodo_normal = new node<vocabulary>;
+            nuevo_nodo_normal->key = meta_nodo->key;
+            for (auto nodos_finales : final_states)
+            {
+                if (std::find(meta_nodo->identity.begin(), meta_nodo->identity.end(), nodos_finales.second) != meta_nodo->identity.end())
+                {
+                    new_finales.insert({nuevo_nodo_normal->key, nuevo_nodo_normal});
+                }
+            }
+            new_nodos.insert({nuevo_nodo_normal->key, nuevo_nodo_normal});
+        }
+
+        for (auto meta_nodo : tabla)
+        {
+            for (auto adj : possible)
+            {
+                for (auto nodos : tabla)
+                {
+                    if (nodos->identity == meta_nodo->adjacent[adj])
+                    {
+                        new_nodos[meta_nodo->key]->adjacent.push_back({adj,new_nodos[nodos->key]});
+                    }
+                }
+            }
+            
+        }
+        states = new_nodos;
+        final_states  = new_finales;
+        for (auto adj : new_nodos[0]->adjacent)
+        {
+            if (adj.first == 'e')
+            {
+                initial_state = adj.second;
             }
         }
-        //Complexity so far O(n + 2n + 4n^2)
-        
-    
+
+        //Finally delete the new zero state and the void state
+        remove_state(0);
+        remove_state(1);
+    }
+    void Brzozowski()
+    {
+        invert();
+        add_state(-1);
+        for (auto final : final_states)
+        {
+            connect_states(-1,final.second->key,'e');
+        }
+        final_states.clear();
+        final_states.insert({initial_state->key,initial_state});
+        initial_state = states[-1];
+        nfa_to_dfa(possible_inputs);
+        invert();
+    }
+    void invert()
+    {
+        std::unordered_map<int, std::vector<std::pair<vocabulary,node<vocabulary>*>>> temp_states; 
+        for (auto vector_data: states)
+        {
+            std::vector<std::pair<vocabulary,node<vocabulary>*>> new_vector;
+            temp_states.insert({vector_data.second->key,new_vector});
+        }
+        for (auto estado : states)
+        {
+            for (auto adj : estado.second->adjacent)
+            {
+                temp_states[adj.second->key].push_back({adj.first,estado.second}); 
+            }
+        }
+        for (auto state: states)
+        {
+            state.second->adjacent = temp_states[state.second->key];
+        }
+
     }
 
+    void print()
+    {
+        for (auto estados : states)
+        {
+            if (final_states.find(estados.second->key) != states.end())
+            {
+                std::cout << "*";
+            }
+            if (initial_state == estados.second)
+            {
+                std::cout << "->";
+            }
+            std::cout << estados.second->key << " ";
+            std::cout << "{";
+            for (auto adj : estados.second->adjacent)
+            {
+                std::cout << adj.second->key << ", ";
+            }
+            std::cout << "} ";
+        }
+    }
 
 };
 
--- main.cpp
+++ main.cpp
@@ -2,6 +2,18 @@
 
 int main()
 {
-    automata<bool> new_automata;
+    automata<char> new_automata;
+    new_automata.add_state(0);
+    new_automata.add_state(1);
+    new_automata.add_state(2);
+    new_automata.initial_state = new_automata.states[0];
+    new_automata.final_states.insert({1,new_automata.states[1]});
+    new_automata.connect_states(0,1,'1');
+    new_automata.connect_states(0,1,'0');
+    new_automata.connect_states(1,0, '1');
+    new_automata.connect_states(1,0, '0');
+    new_automata.connect_states(2,2,'1');
+    new_automata.connect_states(0,2,'0');
     new_automata.Brzozowski();
+    new_automata.print();
 }
